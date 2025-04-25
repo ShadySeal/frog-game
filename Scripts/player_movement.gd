f0c6abdf029e04b26a72ce368588a27e34c34631
ShadySeal <leevi.lehtonen@proton.me>
2025-04-25 20:16:44 +0300

    Commit

--- Scripts/player_movement.gd
+++ Scripts/player_movement.gd
@@ -14,6 +14,14 @@ var jump_charge_time = 0.0
 var charge_percent = 0.0
 var charging_jump = false
 
+var animated_sprite: AnimatedSprite2D
+var animation_player: AnimationPlayer
+
+func _ready() -> void:
+	animated_sprite = $AnimatedSprite2D
+	animation_player = $PlayerAnimation
+	$AnimatedSprite2D/Tongue.set_process(false)
+
 func _physics_process(delta):
 	# Gravity
 	if not is_on_floor():
@@ -21,13 +29,18 @@ func _physics_process(delta):
 
 	# Handle charging while on floor
 	if is_on_floor():
-		if Input.is_action_pressed("Jump"):
+		if Input.is_action_just_pressed("Jump"):
+			velocity.y = min_jump_velocity
+			animation_player.play("jump")
+		
+		if Input.is_action_pressed("Crouch"):
 			charging_jump = true
 			jump_charge_time += delta
 			jump_charge_time = min(jump_charge_time, max_charge_time)
 			charge_percent = jump_charge_time / max_charge_time
+			animation_player.play("charge")
 			emit_signal("charge_updated", charge_percent)
-		elif Input.is_action_just_released("Jump") and charging_jump:
+		elif Input.is_action_just_released("Crouch") and charging_jump:
 			emit_signal("charge_updated", charge_percent)
 			velocity.y = lerp(min_jump_velocity, max_jump_velocity, charge_percent)
 			charging_jump = false
@@ -41,12 +54,29 @@ func _physics_process(delta):
 
 	# Horizontal movement
 	var direction = Input.get_axis("MoveLeft", "MoveRight")
-	if direction:
+	if direction and not charging_jump:
 		velocity.x = direction * speed
+		animated_sprite.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
+	
+	if Input.is_action_just_pressed("Fire"):
+		shoot()
+	
+	# Decide animation priority
+	if not is_on_floor():
+		animation_player.play("jump")
+	elif charging_jump:
+		animation_player.play("charge")
+	elif direction != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("idle")
 
 	move_and_slide()
+
+func shoot() -> void:
+	$AnimatedSprite2D/Tongue.set_process(true)
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("Enemy") or area.is_in_group("Projectile"):
--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -7,7 +7,6 @@ var dir: float
 
 func _ready() -> void:
 	global_position = pos
-	#global_rotation = dir
 	
 func _physics_process(delta: float) -> void:
 	velocity = Vector2(speed, 0).rotated(dir)
